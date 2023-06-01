9404f9d4af57fff18e56fd49ee9134f0cab1998b
truuy <ulangtroy@gmail.com>
2023-06-01 17:19:17 +0800

    TODO: Continue Dashboard design

--- ExpenseTrackerUI/src/app/dashboard/dashboard.component.ts
+++ ExpenseTrackerUI/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Chart} from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements OnInit {
+  ngOnInit() {
+    const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+    const myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Category 1', 'Category 2', 'Category 3'],
+        datasets: [{
+          label: 'Expenses',
+          data: [100, 200, 150],
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 }
+
