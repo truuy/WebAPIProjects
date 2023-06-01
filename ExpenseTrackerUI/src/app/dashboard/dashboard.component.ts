daebd378c77865296a70eba2cda9a58896d63494
truuy <ulangtroy@gmail.com>
2023-06-02 01:54:47 +0800

    Revert "TODO: Continue Dashboard design"
    This reverts commit 9404f9d4af57fff18e56fd49ee9134f0cab1998b.

--- ExpenseTrackerUI/src/app/dashboard/dashboard.component.ts
+++ ExpenseTrackerUI/src/app/dashboard/dashboard.component.ts
@@ -1,32 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import {Chart} from 'chart.js';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
-  ngOnInit() {
-    const ctx = document.getElementById('myChart') as HTMLCanvasElement;
-    const myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Category 1', 'Category 2', 'Category 3'],
-        datasets: [{
-          label: 'Expenses',
-          data: [100, 200, 150],
-        }]
-      },
-      options: {
-        responsive: true,
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    });
-  }
-}
+export class DashboardComponent {
 
+}
